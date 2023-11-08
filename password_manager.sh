5c7054649494cc8f5b8f2079c28f19bbde562f5b
github yosshi-kataoka <github>
2023-11-09 07:46:51 +0900

    パスワード情報を暗号化する処理をpassword_manager.shに追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,8 +1,21 @@
-!#/bin/bash
+!#/bin/bashh
 echo "パスワードマネージャーへようこそ！"
 while true; do
 input=""
-read -p "次の選択肢から入力してください(Add Password/Get Password/Exit):" input
+
+#1~3までの数字を判定
+while true; do
+echo "次の選択肢より、1~3の数字を一つを入力して下さい"
+echo "1:Add Password"
+echo "2:Get Password"
+echo "3:Exit"
+read input
+if [ "$input" =~ ^[1-3]$ ]; then
+break
+echo "入力された値が、1~3の数字ではありません"
+fi
+done
+
 case "$input" in
 "Add Password")
 read -p "サービス名を入力してください:" service
@@ -10,13 +23,18 @@ read -p "ユーザー名を入力してください:" user
 read -p "パスワードを入力してください:" password
 echo
 echo "パスワードの追加は成功しました。"
-echo "サービス名:$service	ユーザー名:$user	パスワード:$password" >> Customer_list;;
+gpg -d PasswordList.gpg > StashFile.txt 2> /dev/null
+echo "サービス名:$service\tユーザー名:$user\tパスワード:$password" >> StashFile.txt
+rm PasswordList.gpg
+gpg -c -o PasswordList.gpg StashFile.txt;;
 
 "Get Password")
 read -p  "サービス名を入力してください:" serch
-result=($(grep "$serch" ./Customer_list))
+gpg -d PasswordList.gpg > StashFile.txt 2> /dev/null
+result=($(grep "$serch" ./StashFile.txt ))
 if [ -n "$result" ]; then
-grep "$serch" ./Customer_list | tr "\t" "\n"
+echo
+grep "$serch" ./StashFile.txt | tr "\t" "\n "
 else echo "そのサービスは登録されていません。"
 fi;;
 
@@ -28,3 +46,4 @@ break;;
 echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。";;
 esac
 done
+rm StashFile.txt
