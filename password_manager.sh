2300885d7c26713331f9ff40990901ee895acee0
github yosshi-kataoka <github>
2023-11-12 18:32:13 +0900

    Password_manager.shを1~3の数字で選択できるようにしました。 Add Password処理に、スペースと空文字のバリデーションを実装しました。 Get Password処理において、StashFile.txtに\t（タブ）で項目を区切っていたが、 スペースに変更しました。

--- password_manager.sh
+++ password_manager.sh
@@ -1,49 +1,69 @@
-!#/bin/bashh
+#!/bin/bash
 echo "パスワードマネージャーへようこそ！"
+item=("サービス名" "ユーザー名" "パスワード")
 while true; do
-input=""
-
-#1~3までの数字を判定
+#1~3の数字を判定
 while true; do
-echo "次の選択肢より、1~3の数字を一つを入力して下さい"
+echo "次の選択肢より、1~3の数字を一つ入力して下さい"
 echo "1:Add Password"
 echo "2:Get Password"
 echo "3:Exit"
 read input
-if [ "$input" =~ ^[1-3]$ ]; then
+if [[ $input =~ ^[1-3]$ ]]; then
 break
 echo "入力された値が、1~3の数字ではありません"
 fi
 done
 
+#選択された数字に応じて処理を実行
 case "$input" in
-"Add Password")
-read -p "サービス名を入力してください:" service
-read -p "ユーザー名を入力してください:" user
-read -p "パスワードを入力してください:" password
-echo
+#Add Password処理
+"1")
+while true; do
+nullFlag=""
+read -r -e -p "サービス名を入力してください:" service
+#スペースを削除
+inputValue=$(echo $service | sed 's/ //g')
+read -r -e -p "ユーザー名を入力してください:" user 
+inputValue=$(echo $user | sed 's/ //g')
+read -r -e -p "パスワードを入力してください:" password
+inputValue=$(echo $password | sed 's/ //g')
+for i in ${inputValue[@]}; do
+#各項目の未入力チェック
+if [ -z ${inputValue[$i]} ]; then
+#入力内容が未入力の場合エラーメッセージを表示
+echo "エラー：${item[$i]}が未入力です。"
+nullFlag=1
+fi
+done
+if [ -z ${nullFlag} ]; then
+break
+fi
+done
+echo ""
 echo "パスワードの追加は成功しました。"
 gpg -d PasswordList.gpg > StashFile.txt 2> /dev/null
-echo "サービス名:$service\tユーザー名:$user\tパスワード:$password" >> StashFile.txt
+echo "サービス名:$service ユーザー名:$user パスワード:$password">> StashFile.txt
 rm PasswordList.gpg
 gpg -c -o PasswordList.gpg StashFile.txt;;
 
-"Get Password")
+#Get Password処理
+"2")
 read -p  "サービス名を入力してください:" serch
 gpg -d PasswordList.gpg > StashFile.txt 2> /dev/null
-result=($(grep "$serch" ./StashFile.txt ))
+result=$(grep "$serch" ./StashFile.txt )
 if [ -n "$result" ]; then
-echo
-grep "$serch" ./StashFile.txt | tr "\t" "\n "
+for service in "$result"; do
+echo "$service" | sed 's/ /\n/g'
+echo ""
+done
 else echo "そのサービスは登録されていません。"
 fi;;
 
-"Exit")
+#Exit処理
+"3")
 echo "Thank you!"
 break;;
-
-*)
-echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。";;
 esac
 done
-rm StashFile.txt
+rm StashFile.txt 2> /dev/null
